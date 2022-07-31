20a58097fec0473c31f5ea4512fd501284a600b0
erhanask <erhanasik8@gmail.com>
2022-07-31 19:44:26 +0300

    Finished

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,33 @@
 import React from 'react'
 
-import { ExampleComponent } from 'erasui'
+import { ErasButton } from 'erasui'
 import 'erasui/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+  <div style={{display:"flex",justifyContent:"space-between",alignItems:"center",padding:"200px"}}>
+    <div>
+      Primary
+    <ErasButton erastyle={{margin: '20px'}} text={`Test Text`} type={'primary'} />
+    </div>
+    <div>
+      Default
+    <ErasButton erastyle={{margin: '20px'}} text={`Test Text`} type={'default'} />
+    </div>
+    <div>
+      Text
+    <ErasButton erastyle={{margin: '20px'}} text={`Test Text`} type={'text'} />
+    </div>
+    <div>
+      Dashed
+    <ErasButton erastyle={{margin: '20px'}} text={`Test Text`} type={'dashed'} />
+    </div>
+    <div>
+      Link
+    <ErasButton erastyle={{margin: '20px'}} text={`Test Text`} type={'link'} />
+    </div>
+  </div>
+  );
 }
 
 export default App
